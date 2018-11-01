a1ada34444b7243eb08616dd3dfc19ea241e6809
Floris van Beekum <34808312+florisvanbeekum@users.noreply.github.com>
2018-11-01 18:55:58 +0100

    Add files via upload

---
+++ Wifi_sensor_V1.ino
@@ -0,0 +1,104 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "Reef19";
+const char* password = "fl70an73ma03da05";
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.2.6";
+const char* mqtt_server = "192.168.2.6";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+float temperature = 0;
+
+// LED Pin
+const int ledPin = 4;
+
+void setup() {
+  Serial.begin(115200);
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+
+    temperature = 20;   
+  
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temperature, 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    char String1[50] = "\"Sensor_ID\": \"Woonkamer1\", \"Temperature\": \"28.17\"";
+//    char String1[100] = "Sensor_ID: Woonkamer1, Temperature: 28.17";
+    Serial.println(String1); 
+    client.publish("Home/Temperature/Woonkamer1", String1);
+
+    
+  }
+}
